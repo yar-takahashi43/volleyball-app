f487420e7f9b709c61b5d972679af12215ed9624
yar-takahashi43 <reeeei.325@gmail.com>
2024-03-25 12:37:37 +0900

    フロントの統合

---
+++ volleyball-front/src/pages/playerList/PlayerDetailsTop.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import HomeIcon from '@mui/icons-material/Home';
+import "../home/Top.css"
+import { Link } from 'react-router-dom';
+
+export default function PlayerDetailsTop() {
+    return (
+        <div className="top">
+            <div className='topLeft'>
+                <div className="topBack">
+                    <ArrowBackIcon className='backIcon'/>
+                <span className="iconName">前ページ</span>
+                </div>
+            </div>
+            <div className="topName">
+                <span className="name">選手情報詳細</span>
+            </div>
+            <div className='topRight'>
+                <div className='topForward'>
+                    <Link to="/" style={{textDecoration: 'none', color: 'white'}}>
+                        <HomeIcon />
+                        <span className='iconName'>TOPへ</span>
+                    </Link>
+                </div>
+            </div>
+        </div>
+      )
+    }
+    
